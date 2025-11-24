2907b5624a3e60b773104e51481e13ba4d1eb7b6
shivagone77 <shivakrishnagone333@gmail.com>
2025-11-24 14:17:20 +0530

    Update Add.java

--- Add.java
+++ Add.java
@@ -2,7 +2,7 @@ public class Add {
     public static void main(String[] args) {
         int a = 5;
         int b = 3;
-        int c = a + b;
+        int sum = a + b;
         System.out.println("sum of"+a "and" +b "is"+sum);
     }
 }
